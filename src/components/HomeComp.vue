2b9c13f6f8b0d547cb0891ece241831f714fb755
alejoquintana <80052934+alejoquintana@users.noreply.github.com>
2023-11-15 12:39:37 -0300

    Update HomeComp.vue

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -12,7 +12,7 @@ function sumAmount(id,sum) {
     <btnBalance></btnBalance>
     <div v-for="product in productStore().products" :key="product.id"
         class="row my-1 p-1 list-item align-items-center g-0" :class="product.brand">
-        <div class="col-2 text-center px-2">
+        <div class="col-3 text-center px-2">
             <span class="text-uppercase font-weight-bold">
                 {{ product.brand }}
             </span>
@@ -22,7 +22,7 @@ function sumAmount(id,sum) {
                 {{ product.name }}
             </span>
         </div>
-        <div class="col-4 d-flex justify-content-between align-items-center">
+        <div class="col-3 d-flex justify-content-between align-items-center">
             <button class="btn btn-danger btn-sm" @click="sumAmount(product.id,false)">
                 <icon icon="minus" size="sm" />
             </button>
