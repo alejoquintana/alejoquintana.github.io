73a1dc2a7326a31cba649c996361048d01540360
alejoquintana <80052934+alejoquintana@users.noreply.github.com>
2024-02-07 13:59:45 -0300

    Update data.js

--- src/stores/data.js
+++ src/stores/data.js
@@ -22,18 +22,21 @@ export const productStore = defineStore('product', () => {
         })
     }
     async function generateBalance() {
+        let ganancia = 0
         let URL = "https://api.whatsapp.com/send?phone=5491165083760&text="
         products.value.map((prod) => {
             if (!balance.value[prod.brand]) {
                 balance.value[prod.brand] = 0
             }
             balance.value[prod.brand] += prod.amount * prod.cost
+            ganancia += prod.amount * (prod.final - prod.cost)
         })
         //! BREAK LINE => %0A
         //! SPACE => %20
         for (const [key, value] of Object.entries(balance.value)) {
             URL = URL + `${key.toUpperCase()}%3A%20%24${value}%0A`
         }
+        URL = URL + `GANANCIA%3A%20%24${ganancia}%0A`
         window.open(URL, "_blank")
     }
 
